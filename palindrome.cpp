2231c36b1f14e44ab3dece8194ba438ba041d8ae
Logan8 <Logan8@gmail.com>
2013-05-15 23:56:48 +0200

    Update palindrome.cpp
    Neil Logan, Palindrome Test:
    
    I simply used a loop to check the first and last letter were the same, then the second and second last letter, and so on.

--- palindrome.cpp
+++ palindrome.cpp
@@ -4,18 +4,45 @@
  *
  * Trevor Fountain
  * April 2013
+ *
+ * Edited by Neil Logan
  */
 
 #include <stdio.h>
+#include <string>
 
 bool isPalindrome(const char *string)
 {
-	// Implement me!
-	return false;
+	//get the lengt of the string
+	int size = strlen(string)-1;
+	
+	//For each letter in the string...
+	for(int letter=0; letter<size;letter++)
+	{
+		//...check the letter in a position from the
+		//front against the letter in the relevant
+		//position from the back
+		if(string[letter]==string[size-letter])
+		{
+			//If letters are the same, continue to next
+			continue;
+		}
+		else
+		{
+			//If letters are different, return false
+			return false;
+		}
+	}
+	
+	//If loop completes, its a palindrome!
+	return true;
 }
 
 int main(int argc, char **argv)
 {
+	//The commented out sections were used to test the statement 
+	//"Note: "abccba" and "abcba" are palindromes; "ABCcba" is not"
+
 	bool pass = 
 		isPalindrome("racecar") &&
 		!isPalindrome("notapalindrome") &&
@@ -23,7 +50,15 @@ int main(int argc, char **argv)
 		!isPalindrome("((((())))))") &&
 		isPalindrome("FOO OOF");
 
+	/*bool pass1 = isPalindrome("abccba");
+	bool pass2 = isPalindrome("abcba");
+	bool pass3 = isPalindrome("ABCcba");*/
+
 	printf("%s\n", pass ? "PASS" : "FAIL");
+
+	/*printf("%s\n", pass1 ? "PASS" : "FAIL");
+	printf("%s\n", pass2 ? "PASS" : "FAIL");
+	printf("%s\n", pass3 ? "PASS" : "FAIL");*/
 
 	return 0;
 }
